7828c4505cf77086a24d7226f58c139aba406aa1
mingxianwei <mingxianwei007@gmail.com>
2022-10-14 15:50:19 +0800

    初始化 项目

---
+++ SwiftUIApp/SwiftUIApp/SwiftUIAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIAppApp.swift
+//  SwiftUIApp
+//
+//  Created by 明先伟 on 2022/10/14.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
