d9af9c41f9c2bb1dfd445ad2e0f0a6f665ed264c
mingxianwei <mingxianwei007@gmail.com>
2022-11-08 12:36:40 +0800

    开始导航条自定义

---
+++ SwiftUIApp/SwiftUIApp/CustomNavigationBar/AppNavBarView.swift
@@ -0,0 +1,20 @@
+//
+//  AppNavBarView.swift
+//  SwiftUIApp
+//
+//  Created by 明先伟 on 2022/11/7.
+//
+
+import SwiftUI
+
+struct AppNavBarView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct AppNavBarView_Previews: PreviewProvider {
+    static var previews: some View {
+        AppNavBarView()
+    }
+}
